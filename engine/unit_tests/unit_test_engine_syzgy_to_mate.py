662620086c78768bd6ff918727c477be374ad378
littlecapa <littlecapa@goolemail.com>
2023-08-09 18:53:01 +0200

    Using log Lib now!

--- engine/unit_tests/unit_test_engine_syzgy_to_mate.py
+++ engine/unit_tests/unit_test_engine_syzgy_to_mate.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('..')
-sys.path.append('/Users/littlecapa/GIT/python/ai_chess_engine_and_trainer')
+sys.path.append('../libs')
 import unittest
 from chess_engine_syzygy import ChessEngineSyzygy
-from libs.log_lib import setup_logging
+from log_lib import setup_logging
 import logging
 from chess import Move
 
@@ -20,14 +20,21 @@ class Tests_Engine_SYZYGY(unittest.TestCase):
         self.assertEqual(eval, 5700.0)
         self.assertEqual(move.uci(), "a8g8")
         self.assertEqual(best_distance, 19)
-        # Flip Color
-        
+
         fen_str = "R7/1K6/8/8/8/8/8/7k b - - 0 1"
         self.engine.setup_from_fen(fen_str)
         eval, move, best_distance = self.engine.get_best_move()
         self.assertEqual(eval, -5200.0)
         self.assertEqual(move.uci(), "h1g2")
         self.assertEqual(best_distance, -24)
+ 
+def setup_logging():
+    logging.basicConfig(
+        filename='app.log',  # Change this to your desired log file path
+        level=logging.INFO,  # Change the log level as needed (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+        format='%(asctime)s [%(levelname)s]: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
 if __name__ == '__main__':
     setup_logging()
