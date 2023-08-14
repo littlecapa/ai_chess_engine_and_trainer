7285f5b1c1a9b1360f05c21f72334d57af40e6a0
littlecapa <littlecapa@goolemail.com>
2023-08-14 16:22:32 +0200

    Implemented first Bitboard Functions

--- libs/chess_lib.py
+++ libs/chess_lib.py
@@ -1,4 +1,5 @@
 import chess
+import numpy as np
 import logging
 import random
 
@@ -24,3 +25,52 @@ def rank_file_from_square(square):
     rank = chess.square_rank(square)
     file = chess.square_file(square)
     return rank, file
+
+def square_from_rank_file(rank, file):
+    return chess.square(file, rank)
+
+def str_square_from_rank_file(rank, file):
+    square_index = square_from_rank_file(rank, file)
+    return chess.square_name(square_index)
+
+def get_zero_bit_vector(length = 8):
+    return np.zeros(length, dtype=bool)
+
+def turn_and_en_passant_square_to_bits(board):
+    bit_vector = get_zero_bit_vector(8)
+    bit_vector[0] = board.turn    
+    en_passant_square = board.ep_square
+    if en_passant_square is not None:
+        bit_vector[1] = 1
+        rank, file = rank_file_from_square(en_passant_square)
+        bit_vector = int_to_3bit_vector(file, bit_vector, 2)
+    return bit_vector
+
+def int_to_3bit_vector(n, vector, start):
+    for i in range(start + 2, start-1, -1):  # Loop from the most significant bit to the least significant bit
+        vector[i] = n & 1
+        n >>= 1
+    return vector
+    
+
+def bit_vector_to_int(bit_vector):
+    result = 0
+    for bit in bit_vector:
+        result = (result << 1) | bit
+    return result
+
+def turn_and_en_passant_square_bits_to_str(bit_vector):
+    if bit_vector[0]:
+        string = "White "
+    else:
+        string = "Black "
+    if bit_vector[1] == 1:
+        file_bits = bit_vector[2:5]
+        file = bit_vector_to_int(file_bits)
+        if bit_vector[0] == 1:
+            rank = (6-1)
+        else:
+            rank = (3-1)
+        square_index = chess.square(file, rank)
+        string += f"EP: {chess.square_name(square_index)}"
+    return string
--- libs/unit_tests/unit_test_chess.py
+++ libs/unit_tests/unit_test_chess.py
@@ -1,7 +1,8 @@
 import unittest
-from libs.chess_lib import uci_to_square, rank_file_from_square
+from libs.chess_lib import uci_to_square, rank_file_from_square, str_square_from_rank_file, get_zero_bit_vector, turn_and_en_passant_square_bits_to_str, turn_and_en_passant_square_to_bits
 from libs.log_lib import setup_logging
 import logging
+import chess
 
 class Tests_Eval_Mate(unittest.TestCase):
 
@@ -14,6 +15,38 @@ class Tests_Eval_Mate(unittest.TestCase):
         self.assertEqual(uci_to_square("d5"), 35)
         self.assertEqual(uci_to_square("e4"), 28)
         self.assertEqual(uci_to_square("e5"), 36)
+
+    def test_square_from_rank_file(self):
+        self.assertEqual(str_square_from_rank_file(0,0), "a1")
+        self.assertEqual(str_square_from_rank_file(7,7), "h8")
+        self.assertEqual(str_square_from_rank_file(4,3), "d5")
+        self.assertEqual(str_square_from_rank_file(3,4), "e4")
+
+    def test_turn_and_en_passant_square_bits_to_str(self):
+        bit_vector = get_zero_bit_vector()
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "Black ")
+        bit_vector[0] = 1
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "White ")
+        bit_vector[1] = 1
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "White EP: a6")
+        bit_vector[4] = 1
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "White EP: b6")
+        bit_vector[3] = 1
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "White EP: d6")
+        bit_vector[0] = 0
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "Black EP: d3")
+        bit_vector[2] = 1
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "Black EP: h3")
+
+    def test_turn_and_en_passant_square_to_bits(self):
+        fen_with_en_passant = "rnbqkbnr/ppppp2p/5p2/5p2/8/8/PPPPPPPP/RNBQKBNR w KQkq f6 0 3"
+        board = chess.Board(fen=fen_with_en_passant)
+        bit_vector = turn_and_en_passant_square_to_bits(board)
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "White EP: f6")
+        fen_with_en_passant = "rnbqkbnr/ppppp1pp/5p2/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 3"
+        board = chess.Board(fen=fen_with_en_passant)
+        bit_vector = turn_and_en_passant_square_to_bits(board)
+        self.assertEqual(turn_and_en_passant_square_bits_to_str(bit_vector), "Black EP: e3")
 
     def test_get_rank_file(self):
         square = uci_to_square("a1")
